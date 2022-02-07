29b39bcf9b7504864afdf0ae38e42105e7c587b2
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-07 21:34:21 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -48,6 +48,6 @@ int main() {
   int(*ChargeRate_FP)(float) = ChargeRate;
   
   
-    assert(batteryIsOk(Temperature_Range(25), SOC(70), ChargeRate(0.7));
+    assert(batteryIsOk(Temperature_Range(25), SOC(70), ChargeRate(0.7)));
 //  assert(!batteryIsOk(50, 85, 0));
 }
