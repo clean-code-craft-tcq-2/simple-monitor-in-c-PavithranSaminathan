7fec0f4709068784770e1a85a8acf830dd59271c
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-08 19:46:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,17 +1,6 @@
-#include <stdio.h>
+#include <BMS.h>
 #include <assert.h>
 
-#define TEMP_LOW_THRESHOLD     (float)0U
-#define TEMP_HIGH_THRESHOLD   (float)45U
-
-#define SOC_LOW_THRESHOLD     (float)20U
-#define SOC_HIGH_THRESHOLD    (float)80U
-
-#define DEFAULT_CHARGE_RATE   (float)0.8
-
-int (*Temperature_Range_FP)(float);
-int (*SOC_FP)(float);
-int(*ChargeRate_FP)(float);
 
 int Temperature_Range(float Temp_F )
 {
