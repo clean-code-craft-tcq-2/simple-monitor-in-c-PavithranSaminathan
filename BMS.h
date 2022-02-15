16abe9d39632f11ce3190c287cbac52e9624dce4
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-15 20:59:07 +0530

    Update BMS.h

--- BMS.h
+++ BMS.h
@@ -3,12 +3,20 @@
 
 #include <stdio.h>
 
-#define TEMP_LOW_THRESHOLD     (float)0U
-#define TEMP_HIGH_THRESHOLD   (float)45U
+typedef unsigned char  bool;
 
+#define TRUE     1
+#define FALSE    0
+
+#define TEMP_WARNING_TOLERANCE  4
+#define TEMP_LOW_THRESHOLD     (float)0U
+#define TEMP_HIGH_THRESHOLD    (float)45U
+
+#define SOC_WARNING_TOLERANCE   4
 #define SOC_LOW_THRESHOLD     (float)20U
 #define SOC_HIGH_THRESHOLD    (float)80U
 
+#define TOLERANCE_CHARGE_RATE   float(0.2)
 #define DEFAULT_CHARGE_RATE   (float)0.8
 
 int (*Temperature_Range_FP)(float);
