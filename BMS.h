b2ef29137d3066c176597c25874f64805306b6d4
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-16 21:22:21 +0530

    Update BMS.h

--- BMS.h
+++ BMS.h
@@ -8,9 +8,7 @@ typedef unsigned char  bool;
 #define TRUE     1
 #define FALSE    0
 
-#define TEMP_WARNING_TOLERANCE  4
-#define TEMP_LOW_THRESHOLD     (float)0U
-#define TEMP_HIGH_THRESHOLD    (float)45U
+
 
 #define SOC_WARNING_TOLERANCE   4
 #define SOC_LOW_THRESHOLD     (float)20U
@@ -19,8 +17,7 @@ typedef unsigned char  bool;
 #define TOLERANCE_CHARGE_RATE  (float)0.2
 #define DEFAULT_CHARGE_RATE    (float)0.8
 
-bool (*TempPrintWarning_FP)(float ATemp_F);
-bool (*TempPrintAlarm_FP)(float ATemp_F);
+
 bool (*SOCPrintWarning_FP)(float SOC_F);
 bool (*SOCPrintAlarm_FP)(float SOC_F);
 bool (*CheckChargeRatePrintWarning_FP)(float chargeRate_F);
