a0eb322c009867e7791e2690d9f07228328fc619
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-16 22:01:33 +0530

    Update BMS.h

--- BMS.h
+++ BMS.h
@@ -8,10 +8,5 @@ typedef unsigned char  bool;
 #define TRUE     1
 #define FALSE    0
 
-#define TOLERANCE_CHARGE_RATE  (float)0.2
-#define DEFAULT_CHARGE_RATE    (float)0.8
-
-bool (*CheckChargeRatePrintWarning_FP)(float chargeRate_F);
-bool (*CheckChargeRatePrintAlarm_FP)(float chargeRate_F);
 
 #endif
