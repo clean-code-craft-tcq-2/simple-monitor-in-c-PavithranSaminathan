ea7452ef84ad2ad2eb9f1ddfbb72c85b01917fad
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-16 21:31:16 +0530

    Create PrintLanguage.h

---
+++ PrintLanguage.h
@@ -0,0 +1,12 @@
+#ifndef PrintLanguage_h
+#define PrintLanguage_h
+
+
+#define LANGUAGE ENGLISH
+
+extern const char *Notify[];
+extern const char *Alarm[];
+
+extern void PrintData(const char *data)
+
+#endif
