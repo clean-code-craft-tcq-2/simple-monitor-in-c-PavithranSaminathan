9c6ce6b62fd5fb43a63b8324c072ea36e67ae25b
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-16 21:22:17 +0530

    Create TempMonitor.h

---
+++ TempMonitor.h
@@ -0,0 +1,18 @@
+#ifndef TempMonitor_H
+#define TempMonitor_H
+
+#include "BMS.h"
+
+#define TEMP_WARNING_TOLERANCE  4
+#define TEMP_LOW_THRESHOLD     (float)0U
+#define TEMP_HIGH_THRESHOLD    (float)45U
+
+bool TempPrintWarning(float ATemp_F);
+bool TempPrintAlarm(float ATemp_F);
+
+extern bool CheckTemperatureRange(float ATemp_F,bool (*TempPrintWarning_FP)(float), bool (*TempPrintAlarm_FP)(float));
+
+bool (*TempPrintWarning_FP)(float ATemp_F);
+bool (*TempPrintAlarm_FP)(float ATemp_F);
+
+#endif
