b41cffe4140489399f988dd1742dbc1d618bd4be
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-16 23:47:41 +0530

    Update BMS.c

--- BMS.c
+++ BMS.c
@@ -44,6 +44,10 @@ int main()
  
   /*Assert function to check SOC function*/    
   Test_CheckSOC(19,FALSE);
+  Test_CheckSOC(20,TRUE);
+  Test_CheckSOC(45,TRUE);
+  Test_CheckSOC(79,TRUE);
+  Test_CheckSOC(80,FALSE);
   
   Test_SOCPrintWarning(19,TRUE);
   Test_SOCPrintWarning(20,TRUE);
