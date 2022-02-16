144bac766b754c25fb5bc5abc2bf5bbe7fca9b27
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-16 22:01:27 +0530

    Update BMS.c

--- BMS.c
+++ BMS.c
@@ -2,55 +2,9 @@
 #include "TempMonitor.h"
 #include "PrintLanguage.h"
 #include "SOCMonitor.h"
+#include "ChargeRateMonitor.h"
 
 #include <assert.h>
-
-
-
-bool CheckChargeRatePrintWarning(float chargeRate_F)
-{
-   if((DEFAULT_CHARGE_RATE - TOLERANCE_CHARGE_RATE)< chargeRate_F && (DEFAULT_CHARGE_RATE > chargeRate_F ))
-  {
-    PrintData(Notify[2]);
-    return TRUE;
-  }
-  else
-  {
-    return FALSE;
-  }
-}
-
-
-bool CheckChargeRatePrintAlarm(float chargeRate_F)
-{
-   if(DEFAULT_CHARGE_RATE < chargeRate_F )
-  {
-    PrintData(Alarm[2]);
-    return FALSE;
-  }
-  else
-  {
-    return TRUE;
-  }
-}
-
-/*To check ChargeRate violation */
-bool CheckChargeRate(float chargeRate_F,bool (*CheckChargeRatePrintWarning_FP)(float), bool (*CheckChargeRatePrintAlarm_FP)(float))
-{
-  if(CheckChargeRatePrintWarning_FP(chargeRate_F))
-  {
-    return TRUE;
-  }
-  else
-  {
-    return (CheckChargeRatePrintAlarm_FP(chargeRate_F));
-  }
-}
-
-//bool batteryIsOk(float ATemp_F,float Asoc_F, float AChargeRate_F,bool (*Temperature_Range_FP)(float) , bool (*SOC_FP)(float) , bool(*ChargeRate_FP)(float)) 
-//{    
-//  return (Temperature_Range_FP(ATemp_F) && SOC_FP(Asoc_F) && ChargeRate_FP(AChargeRate_F));   
-//}
 
 bool batteryIsOk(float ATemp_F,float Asoc_F, float AChargeRate_F,
                  bool (*SOCPrintWarning_FP)(float), bool (*SOCPrintAlarm_FP)(float),
@@ -67,8 +21,7 @@ bool batteryIsOk(float ATemp_F,float Asoc_F, float AChargeRate_F,
 int main() 
 {  
   
-bool (*CheckChargeRatePrintWarning_FP)(float chargeRate_F)=CheckChargeRatePrintWarning;
-bool (*CheckChargeRatePrintAlarm_FP)(float chargeRate_F)=CheckChargeRatePrintAlarm;
+
 
 //bool (*Temperature_Range_FP)(float)= CheckTemperatureRange;
 //bool (*SOC_FP)(float)=CheckSOC;
