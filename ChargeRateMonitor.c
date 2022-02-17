8c120953ab50ebb360ec93fa0c0ca21b8404a66b
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-17 12:24:46 +0530

    Update ChargeRateMonitor.c

--- ChargeRateMonitor.c
+++ ChargeRateMonitor.c
@@ -1,4 +1,5 @@
 #include "ChargeRateMonitor.h"
+#include <assert.h>
 
 bool (*CheckChargeRatePrintWarning_FP)(float chargeRate_F)=CheckChargeRatePrintWarning;
 bool (*CheckChargeRatePrintAlarm_FP)(float chargeRate_F)=CheckChargeRatePrintAlarm;
@@ -41,3 +42,29 @@ bool CheckChargeRate(float chargeRate_F,bool (*CheckChargeRatePrintWarning_FP)(f
     return (CheckChargeRatePrintAlarm_FP(chargeRate_F));
   }
 }
+
+
+#if(WORK_ENVIRONMENT == TEST)
+
+bool Test_CheckChargeRate(float chargeRate_F,bool Expected_Result )
+{
+   bool Test_Result;
+   Test_Result=CheckChargeRate( chargeRate_F,CheckChargeRatePrintWarning_FP,CheckChargeRatePrintAlarm_FP);
+   assert(Expected_Result == Test_Result);
+}
+
+bool Test_CheckChargeRatePrintAlarm(float chargeRate_F,bool Expected_Result )
+{
+   bool Test_Result;
+   Test_Result=CheckChargeRatePrintAlarm(chargeRate_F);
+   assert(Expected_Result == Test_Result);
+}
+
+bool Test_CheckChargeRatePrintWarning(float chargeRate_F,bool Expected_Result )
+{
+   bool Test_Result;
+   Test_Result=CheckChargeRatePrintWarning(chargeRate_F);
+   assert(Expected_Result == Test_Result);
+}
+
+#endif
