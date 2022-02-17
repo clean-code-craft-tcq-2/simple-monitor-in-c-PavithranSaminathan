dd5afafa1fa4dddb214225c503b15a4997a3f07a
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-02-17 13:07:10 +0530

    Update SOCMonitor.c

--- SOCMonitor.c
+++ SOCMonitor.c
@@ -5,6 +5,8 @@
 bool (*SOCPrintWarning_FP)(float SOC_F)=SOCPrintWarning;
 bool (*SOCPrintAlarm_FP)(float SOC_F)=SOCPrintAlarm;
 
+/** Brief :- This function will print warning message in case of SOC not inbetween 24 to 76 value*/
+
 bool SOCPrintWarning(float SOC_F)
 {
 if(((SOC_LOW_THRESHOLD + SOC_WARNING_TOLERANCE) > SOC_F) ||
@@ -18,6 +20,8 @@ if(((SOC_LOW_THRESHOLD + SOC_WARNING_TOLERANCE) > SOC_F) ||
     return FALSE;
   }
 }
+
+/** Brief :- This function will print alarm message in case of SOC not inbetween 20 to 80 value*/
 
 bool SOCPrintAlarm(float SOC_F)
 {
@@ -33,7 +37,8 @@ if((SOC_LOW_THRESHOLD > SOC_F) || (SOC_HIGH_THRESHOLD < SOC_F))
 }
 
 
-/*To check State of Charge violation */
+/** Brief :- This function will print warning or alarm message in case of SOC not inbetween threshold ranges*/
+
 bool CheckSOC(float SOC_F,bool (*SOCPrintWarning_FP)(float), bool (*SOCPrintAlarm_FP)(float))
 {
   if(!SOCPrintAlarm_FP(SOC_F))
@@ -49,6 +54,8 @@ bool CheckSOC(float SOC_F,bool (*SOCPrintWarning_FP)(float), bool (*SOCPrintAlar
 
 #if(WORK_ENVIRONMENT == TEST)
 
+/** Brief :- This test function will check CheckSOC output*/
+
 void Test_CheckSOC(float ASOC,bool Expected_Result)
 {
   bool Test_Result;
@@ -56,12 +63,16 @@ void Test_CheckSOC(float ASOC,bool Expected_Result)
   assert(Expected_Result == Test_Result);
 }
 
+/** Brief :- This test function will check SOCPrintAlarm output*/
+
 bool Test_SOCPrintAlarm(float SOC_F,bool Expected_Result)
 {
   bool Test_Result;
   Test_Result =SOCPrintAlarm(SOC_F);
   assert(Expected_Result == Test_Result);
 }
+
+/** Brief :- This test function will check SOCPrintWarning output*/
 
 bool Test_SOCPrintWarning(float SOC_F,bool Expected_Result)
 {
